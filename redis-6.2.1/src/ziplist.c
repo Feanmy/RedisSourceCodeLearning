f6a2b48addca4346bf91f4f872d6e85a84947fe2
feanmy <gaofeng@yktour.com.cn>
2021-07-08 12:16:32 +0800

    210708-ziplist.c

--- redis-6.2.1/src/ziplist.c
+++ redis-6.2.1/src/ziplist.c
@@ -272,21 +272,33 @@
  * Note that this is not how the data is actually encoded, is just what we
  * get filled by a function in order to operate more easily. */
 typedef struct zlentry {
+
+    /* 编码前一个实体数据长度所需要的字节数 */
     unsigned int prevrawlensize; /* Bytes used to encode the previous entry len*/
+
+    /* 前一个实体数据长度 */
     unsigned int prevrawlen;     /* Previous entry len. */
+
+    /* 编码当前实体数据所需字节数 */
     unsigned int lensize;        /* Bytes used to encode this entry type/len.
                                     For example strings have a 1, 2 or 5 bytes
                                     header. Integers always use a single byte.*/
+
+    /* 实体数据长度 */
     unsigned int len;            /* Bytes used to represent the actual entry.
                                     For strings this is just the string length
                                     while for integers it is 1, 2, 3, 4, 8 or
                                     0 (for 4 bit immediate) depending on the
                                     number range. */
     unsigned int headersize;     /* prevrawlensize + lensize. */
+
+    /* 编码 */
     unsigned char encoding;      /* Set to ZIP_STR_* or ZIP_INT_* depending on
                                     the entry encoding. However for 4 bits
                                     immediate integers this can assume a range
                                     of values and must be range-checked. */
+
+    /* 指向实体起始处 */
     unsigned char *p;            /* Pointer to the very start of the entry, that
                                     is, this points to prev-entry-len field. */
 } zlentry;
@@ -432,7 +444,7 @@ unsigned int zipStoreEntryEncoding(unsigned char *p, unsigned char encoding, uns
 /* Encode the length of the previous entry and write it to "p". This only
  * uses the larger encoding (required in __ziplistCascadeUpdate). */
 int zipStorePrevEntryLengthLarge(unsigned char *p, unsigned int len) {
-    uint32_t u32;
+    uint32_t u32;  /* typedef int uint32_t */
     if (p != NULL) {
         p[0] = ZIP_BIG_PREVLEN;
         u32 = len;
